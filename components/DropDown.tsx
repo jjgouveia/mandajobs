58dcb66359f5e0f6fb02cbdb735cc85b28ff910b
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-07-31 22:37:04 -0300

    feat: linkesearch inputs and query to GPT

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,21 +10,21 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Direto ao ponto" | "Passo a passo";
+export type levelType = "Senior" | "Junior" | "Do not especify";
 
 interface DropDownProps {
-  vibe: VibeType;
-  setVibe: (vibe: VibeType) => void;
+  level: levelType;
+  setLevel: (level: levelType) => void;
 }
 
-let vibes: VibeType[] = ["Direto ao ponto", "Passo a passo"];
+let levels: levelType[] = ["Senior", "Junior", "Do not especify"];
 
-export default function DropDown({ vibe, setVibe }: DropDownProps) {
+export default function DropDown({ level, setLevel }: DropDownProps) {
   return (
     <Menu as="div" className="relative block text-left w-full">
       <div>
         <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-black">
-          {vibe}
+          {level}
           <ChevronUpIcon
             className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
             aria-hidden="true"
@@ -46,20 +46,20 @@ export default function DropDown({ vibe, setVibe }: DropDownProps) {
         leaveTo="transform opacity-0 scale-95">
         <Menu.Items
           className="absolute left-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          key={vibe}>
+          key={level}>
           <div className="">
-            {vibes.map((vibeItem) => (
-              <Menu.Item key={vibeItem}>
+            {levels.map((levelItem) => (
+              <Menu.Item key={levelItem}>
                 {({ active }) => (
                   <button
-                    onClick={() => setVibe(vibeItem)}
+                    onClick={() => setLevel(levelItem)}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      vibe === vibeItem ? "bg-gray-200" : "",
+                      level === levelItem ? "bg-gray-200" : "",
                       "px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between"
                     )}>
-                    <span>{vibeItem}</span>
-                    {vibe === vibeItem ? (
+                    <span>{levelItem}</span>
+                    {level === levelItem ? (
                       <CheckIcon className="w-4 h-4 text-bold" />
                     ) : null}
                   </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRef, useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
-import DropDown, { VibeType } from "../components/DropDown";
+import DropDown, { levelType } from "../components/DropDown";
 import Footer from "../components/Footer";
 import Github from "../components/GitHub";
 import Header from "../components/Header";
@@ -16,8 +16,10 @@ import {
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState("");
-  const [vibe, setVibe] = useState<VibeType>("Direto ao ponto");
+  const [title, setTitle] = useState("");
+  const [tools, setTools] = useState("");
+  const [boringTools, setBoringTools] = useState("");
+  const [level, setLevel] = useState<levelType>("Do not especify");
   const [generatedBios, setGeneratedBios] = useState<String>("");
 
   const bioRef = useRef<null | HTMLDivElement>(null);
@@ -28,12 +30,25 @@ const Home: NextPage = () => {
     }
   };
 
-  const prompt = `Answer the question, ${
-    vibe === "Direto ao ponto"
-      ? "without explanation, just the answer, be quick and short"
-      : "with a full step-by-step explanation"
-  }
-      Make sure that the question is in portuguese, and asnwer in portuguese, here is the question:  ${bio}`;
+  const switchLevel = () => {
+    switch (level) {
+      case "Do not especify":
+        return "";
+        break;
+      case "Senior":
+        return "only Seniors titles";
+        break;
+      case "Junior":
+        return "only Junior titles";
+        break;
+      default:
+        return "";
+    }
+  };
+
+  const prompt = `Create a perfect query for i use in linkedin searchbar, use the opperators AND, OR, NOT e () for that.Just give me the code, without explanation.
+  I'm a ${title} professional that uses ${tools} and don't like to use ${boringTools}. , ${switchLevel()}
+  `;
 
   const generateBio = async (e: any) => {
     e.preventDefault();
@@ -89,64 +104,99 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Exercita AI</title>
+        <title>Linkesearch</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* <Header /> */}
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-6 sm:mt-12">
         <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-purple-500 px-4 py-2 text-sm text-white shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href=""
+          className="flex max-w-fit items-center justify-center space-x-2 rounded-full bg-blue-600 px-4 py-2 text-sm text-white shadow-md transition-colors hover:bg-blue-500 mb-5"
+          href="https://www.linkedin.com/in/daniellimae/"
           target="_blank"
           rel="noopener noreferrer">
-          <p>v0.0.1</p>
+          <p>by @daniellimae</p>
         </a>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Exercita AI
+          Linkesearch
         </h1>
+        <br />
+        <Image
+          src="/searchbar.png"
+          width={400}
+          height={400}
+          alt="1 icon"
+          className="mb-5 sm:mb-0"
+        />
         <p className="text-slate-500 mt-5">
-          47.118 questões respondidas até agora.
+          Search smarter, boost your chances with our LinkedIn assistant
         </p>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
-            <Image
-              src="/1-black.png"
-              width={30}
-              height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
-            />
+            <p className="text-left font-medium text-blue-600">1</p>
             <p className="text-left font-medium">
-              Cole sua questāo
+              Your job title
               <span className="text-slate-500">
-                (ou escreva alguma dúvida sobre qualuqer matéria)
+                ( don't worrie about details, just a word )
               </span>
               .
             </p>
           </div>
-          <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={"Exemplo: Quem veio primeiro, o ovo ou a galinha?"}
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="mt-4 mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder={"Example: Frontend"}
+          />
+          <div className="flex mt-10 items-center space-x-3">
+            <p className="text-left font-medium text-blue-600">2</p>
+            <p className="text-left font-medium">
+              Tools that you use
+              <span className="text-slate-500">
+                ( Languages to code, excel toolkit ... )
+              </span>
+              .
+            </p>
+          </div>
+          <input
+            value={tools}
+            onChange={(e) => setTools(e.target.value)}
+            className="mt-4 mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder={"Example: React, Angular, Javascript..."}
+          />
+          <div className="flex mt-10 items-center space-x-3">
+            <p className="text-left font-medium text-blue-600">3</p>
+            <p className="text-left font-medium">
+              Tools that you wouldn't like to work with.
+            </p>
+          </div>
+          <input
+            value={boringTools}
+            onChange={(e) => setBoringTools(e.target.value)}
+            className="mt-4 mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder={"Example: Php, Java, Javascript..."}
           />
           <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
+            <p className="text-left font-medium text-blue-600">4</p>
             <p className="text-left font-medium">
-              Selecione o modo de resposta
+              Your level{" "}
+              <span className="text-slate-500">
+                ( mid-levels do not especify )
+              </span>
             </p>
           </div>
           <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
+            <DropDown
+              level={level}
+              setLevel={(newlevel) => setLevel(newlevel)}
+            />
           </div>
 
           {!loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className="bg-blue-600 rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-blue-500 w-full"
               onClick={(e) => generateBio(e)}>
-              Gerar sua resposta
+              Improve my linkesearch
             </button>
           )}
           {loading && (
@@ -170,7 +220,7 @@ const Home: NextPage = () => {
                 <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
                   ref={bioRef}>
-                  Sua resposta:
+                  Your query to search:
                 </h2>
               </div>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
@@ -183,7 +233,7 @@ const Home: NextPage = () => {
                         className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
                         onClick={() => {
                           navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
+                          toast("Linkeseach copied to clipboard", {
                             icon: "✂️",
                           });
                         }}
