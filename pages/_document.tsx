db96b0ed39f0019b9e6977370fab55baacfe1637
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-08-16 22:44:11 -0300

    fix: corrigindo as tags do open graph

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,24 +6,35 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta name="title" content="Manda Jobs" />
           <meta
             name="description"
             content="Otimize sua busca por vagas no LinkedIn através da consulta booleana e garanta as melhores oportunidades para o seu perfil."
           />
-          <meta property="og:site_name" content="Manda Jobs" />
+
+          <meta property="og:url" content="https://mandajobs.vercel.app" />
+          <meta property="og:title" content="Manda Jobs" />
           <meta
             property="og:description"
             content="Otimize sua busca por vagas no LinkedIn através da consulta booleana e garanta as melhores oportunidades para o seu perfil."
           />
-          <meta property="og:title" content="Manda Jobs" />
-          <meta name="Manda Jobs:card" content="summary_large_image" />
-          <meta name="Manda Jobs:title" content="Manda Jobs" />
           <meta
-            name="Manda Jobs:description"
+            property="og:image"
+            content="https://github.com/jjgouveia/sturdy-doodle/blob/main/mandajobs/mandajobs.png?raw=true"
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="mandajobs.vercel.app" />
+          <meta property="twitter:url" content="https://mandajobs.vercel.app" />
+          <meta name="twitter:title" content="Manda Jobs" />
+          <meta
+            name="twitter:description"
             content="Otimize sua busca por vagas no LinkedIn através da consulta booleana e garanta as melhores oportunidades para o seu perfil."
           />
-          <meta property="og:image" content="/searchbar.png" />
-          <meta name="Manda Jobs:image" content="/searchbar.png" />
+          <meta
+            name="twitter:image"
+            content="https://github.com/jjgouveia/sturdy-doodle/blob/main/mandajobs/mandajobs.png?raw=true"
+          />
         </Head>
         <body>
           <Main />
