a372faa8123a65ccf82253e49147bf64c4c04192
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-08-20 14:56:07 -0300

    feat: melhorias de SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,13 @@ class MyDocument extends Document {
             property="og:image"
             content="https://github.com/jjgouveia/sturdy-doodle/blob/main/mandajobs/mandajobs.png?raw=true"
           />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:site_name" content="Manda Jobs" />
+          <meta property="og:author" content="@jjgouveia" />
+          <meta property="og:creator" content="@jjgouveia" />
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta property="twitter:domain" content="mandajobs.vercel.app" />
@@ -35,6 +42,19 @@ class MyDocument extends Document {
             name="twitter:image"
             content="https://github.com/jjgouveia/sturdy-doodle/blob/main/mandajobs/mandajobs.png?raw=true"
           />
+          <meta name="twitter:creator" content="@outrogouveia" />
+
+          <meta name="apple-mobile-web-app-title" content="Manda Jobs" />
+          <meta name="application-name" content="Manda Jobs" />
+          <meta name="msapplication-TileColor" content="#111827" />
+          <meta name="msapplication-TileImage" content="/favicon.ico" />
+          <meta name="msapplication-config" content="/favicon.ico" />
+
+          <meta name="theme-color" content="#111827" />
+          <meta name="msapplication-navbutton-color" content="#111827" />
+          <meta itemProp="name" content="Manda Jobs" />
+          <meta itemProp="description" content="Manda Jobs" />
+          <meta itemProp="image" content="/favicon.ico" />
         </Head>
         <body>
           <Main />
--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -33,15 +33,10 @@ const handler = async (req: Request): Promise<Response> => {
   // return stream response (SSE)
   return new Response(
     stream, {
-      headers: new Headers({
-        // since we don't use browser's EventSource interface, specifying content-type is optional.
-        // the eventsource-parser library can handle the stream response as SSE, as long as the data format complies with SSE:
-        // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#sending_events_from_the_server
-        
-        // 'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-      })
-    }
+    headers: new Headers({
+      'Cache-Control': 'no-cache',
+    })
+  }
   );
 };
 
