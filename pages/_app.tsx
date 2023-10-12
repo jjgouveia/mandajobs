cb848c5bf47ca28b0cc4844d29cae201fe033a78
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-10-11 22:36:44 -0300

    feat: aplicando feat de Recuperação de bloqueio de anúncios do Ad Sense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,21 @@ function MyApp({ Component, pageProps }: AppProps) {
       `;
       document.head.appendChild(script2);
     };
+
+    const script3 = document.createElement("script");
+    script3.src =
+      "https://fundingchoicesmessages.google.com/i/pub-2455539118400314?ers=1";
+    script3.nonce = "kk7rRBN16gD-PENUS_VXhA";
+    script3.async = true;
+    document.head.appendChild(script3);
+
+    script3.onload = () => {
+      const script4 = document.createElement("script");
+      script4.innerHTML = `
+        (function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
+      `;
+      document.head.appendChild(script4);
+    };
   }, []);
   return (
     <>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2455539118400314"
             crossOrigin="anonymous"
           ></script>
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
