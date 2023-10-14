01f8e9079950287a50f96b113534b1d040db14d3
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-10-14 13:26:44 -0300

    fix: correção no component Card que possuia uma propriedade target desnecessária

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -10,10 +10,7 @@ export default function Card({
   icon,
 }: Props & React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div
-      target="_blank"
-      className="group block h-full shadow-md rounded-xl border border-slate-500/30 bg-zinc-700/60 p-7"
-    >
+    <div className="group block h-full shadow-md rounded-xl border border-slate-500/30 bg-zinc-700/60 p-7">
       <div className="flex flex-col flex-wrap gap-3">
         <div className="flex items-center gap-3 pb-2">
           <div className="w-12 h-12 flex justify-center items-center border border-slate-500/10 rounded-3xl">
